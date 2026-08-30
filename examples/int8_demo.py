13fc1ea1c0ee5e491e85162025900bac541263ac
Harsh <harshrawat096@gmail.com>
2026-08-30 14:43:04 +0530

    int8 transformation

---
+++ examples/int8_demo.py
@@ -0,0 +1,30 @@
+import numpy as np
+from quantization.symmetric import cal_scale, quantize_tensor, dequantize_tensor
+
+weights = np.array(
+     [1.2, -3.5, 0.8, 2.1, -1.9, 3.5],
+     dtype=np.float32
+)
+
+scale = cal_scale(weights)
+quantized_weights = quantize_tensor(weights, scale)
+dequantized_weights = dequantize_tensor(quantized_weights, scale)
+
+print("Original Weights: ", weights)
+print("Scale Factor: ", scale)
+print("Quantized Weights: ", quantized_weights)
+print("Dequantized Weights: ", dequantized_weights)
+
+#Errors
+
+error = weights - dequantized_weights
+print("Quantization Error: ", error)
+absolute_error = np.abs(error)
+print("Absolute Quantization Error: ", absolute_error)
+mae = np.mean(absolute_error)
+print("Mean Absolute Error (MAE): ", mae)
+mse = np.mean(error ** 2)
+print("Mean Squared Error (MSE): ", mse)
+rmse = np.sqrt(mse)
+print("Root Mean Squared Error (RMSE): ", rmse)
+
---
+++ quantization/symmetric.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+# Step1: Calculate the scale factor for symmetric quantization
+def cal_scale(tensor):
+    abs_max = np.max(np.abs(tensor))
+    scale = abs_max / 127.0
+    return scale
+
+#Step2: Quantize the tensor using the scale factor
+def quantize_tensor(tensor, scale):
+    quantized_tensor = np.round(tensor/scale)
+    quantized_tensor = np.clip(quantized_tensor, -127, 127)
+    return quantized_tensor
+
+#Step3: Dequantize the tensor back to its original form
+def dequantize_tensor(quantized_tensor, scale):
+    return quantized_tensor.astype(np.float32) * scale
